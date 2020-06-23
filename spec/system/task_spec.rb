4d28a9d2d07750c9e23bdd0a43e22ef7edd6b59b
asaki kusano <darumade4@gmail.com>
2020-06-23 23:19:04 +0900

    E2Eテスト記述

---
+++ spec/system/task_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+RSpec.describe 'タスク管理機能', type: :system do
+  describe 'タスク一覧画面' do
+    context 'タスクを作成した場合' do
+      it '作成済みのタスクが表示される' do
+        task = FactoryBot.create(:task)
+        visit tasks_path
+        expect(page).to have_content 'task'
+      end
+    end
+  end
+  describe 'タスク登録画面' do
+    context '必要項目を入力して、createボタンを押した場合' do
+      it 'データが保存される' do
+        visit new_task_path
+        fill_in "task[name]", with: 'test_name'
+        fill_in "task[content]", with: 'test_content'
+        click_button 'commit'
+        expect(page).to have_content 'test_name'
+        expect(page).to have_content 'test_content'
+      end
+    end
+  end
+  describe 'タスク詳細画面' do
+     context '任意のタスク詳細画面に遷移した場合' do
+       it '該当タスクの内容が表示されたページに遷移する' do
+         test3 = FactoryBot.create(:task, name: 'test3_title', content: 'test3_content')
+         visit task_path(test3)
+         expect(page).to have_content 'test3'
+       end
+     end
+  end
+end
