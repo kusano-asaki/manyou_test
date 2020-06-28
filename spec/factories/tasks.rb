a3be08862486f1f68bcd3d0c3107b7f895a8838d
asaki kusano <darumade4@gmail.com>
2020-06-28 16:34:31 +0900

    taskバリデーションテスト作成

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,8 +3,4 @@ FactoryBot.define do
     name { 'task' }
     content { 'Factoryで作ったデフォルトのコンテント１' }
   end
-  # factory :second_task, class: Task do
-  #   name { 'task2' }
-  #   contetnt { 'Factoryで作ったデフォルトのコンテント2' }
-  # end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,44 +1,44 @@
-require 'rails_helper'
-RSpec.describe 'タスク管理機能', type: :system do
-  before do
-    FactoryBot.create(:task)
-    FactoryBot.create(:task, name: 'task2')
-  end
-  describe 'タスク一覧画面' do
-    context 'タスクを作成した場合' do
-      it '作成済みのタスクが表示される' do
-        visit tasks_path
-        expect(page).to have_content 'task'
-      end
-    end
-    context '複数のタスクを作成した場合' do
-      it 'タスクが作成日時の降順に並んでいる' do
-        visit tasks_path
-        task_list = all('.task_row') # タスク一覧を配列として取得するため、View側でidを振っておく
-        expect(task_list[0]).to have_content 'task2'
-        expect(task_list[1]).to have_content 'task'
-      end
-    end
-  end
-  describe 'タスク登録画面' do
-    context '必要項目を入力して、createボタンを押した場合' do
-      it 'データが保存される' do
-        visit new_task_path
-        fill_in "task[name]", with: 'test_name'
-        fill_in "task[content]", with: 'test_content'
-        click_button 'commit'
-        expect(page).to have_content 'test_name'
-        expect(page).to have_content 'test_content'
-      end
-    end
-  end
-  describe 'タスク詳細画面' do
-     context '任意のタスク詳細画面に遷移した場合' do
-       it '該当タスクの内容が表示されたページに遷移する' do
-         test3 = FactoryBot.create(:task, name: 'test3_title', content: 'test3_content')
-         visit task_path(test3)
-         expect(page).to have_content 'test3_title'
-       end
-     end
-  end
-end
+# require 'rails_helper'
+# RSpec.describe 'タスク管理機能', type: :system do
+#   before do
+#     FactoryBot.create(:task)
+#     FactoryBot.create(:task, name: 'task2')
+#   end
+#   describe 'タスク一覧画面' do
+#     context 'タスクを作成した場合' do
+#       it '作成済みのタスクが表示される' do
+#         visit tasks_path
+#         expect(page).to have_content 'task'
+#       end
+#     end
+#     context '複数のタスクを作成した場合' do
+#       it 'タスクが作成日時の降順に並んでいる' do
+#         visit tasks_path
+#         task_list = all('.task_row') # タスク一覧を配列として取得するため、View側でidを振っておく
+#         expect(task_list[0]).to have_content 'task2'
+#         expect(task_list[1]).to have_content 'task'
+#       end
+#     end
+#   end
+#   describe 'タスク登録画面' do
+#     context '必要項目を入力して、createボタンを押した場合' do
+#       it 'データが保存される' do
+#         visit new_task_path
+#         fill_in "task[name]", with: 'test_name'
+#         fill_in "task[content]", with: 'test_content'
+#         click_button 'commit'
+#         expect(page).to have_content 'test_name'
+#         expect(page).to have_content 'test_content'
+#       end
+#     end
+#   end
+#   describe 'タスク詳細画面' do
+#      context '任意のタスク詳細画面に遷移した場合' do
+#        it '該当タスクの内容が表示されたページに遷移する' do
+#          test3 = FactoryBot.create(:task, name: 'test3_title', content: 'test3_content')
+#          visit task_path(test3)
+#          expect(page).to have_content 'test3_title'
+#        end
+#      end
+#   end
+# end
