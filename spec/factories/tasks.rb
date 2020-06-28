dea4ad0a79b5e43d9f46ac6239cf7585abd159ba
asaki kusano <darumade4@gmail.com>
2020-06-28 15:26:22 +0900

    タスクが作成日時の降順に並んでいるテスト記述

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :task do
     name { 'task' }
-    content { 'task' }
+    content { 'Factoryで作ったデフォルトのコンテント１' }
   end
+  # factory :second_task, class: Task do
+  #   name { 'task2' }
+  #   contetnt { 'Factoryで作ったデフォルトのコンテント2' }
+  # end
 end
