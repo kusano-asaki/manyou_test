882cf1d208af5c28873bdf0a431f3a42cc0847d0
asaki kusano <darumade4@gmail.com>
2020-06-29 17:53:00 +0900

    コンフリクト編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ before_action :basic_auth
     def basic_auth
       authenticate_or_request_with_http_basic do |username, password|
         username == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+      end
     end
 end
