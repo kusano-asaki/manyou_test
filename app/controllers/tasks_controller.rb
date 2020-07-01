8cb0c397365ae4863cacb8060cbe35c829ff7599
asaki kusano <darumade4@gmail.com>
2020-07-01 22:50:44 +0900

    終了期限登録フォーム作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -69,6 +69,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:name, :content)
+      params.require(:task).permit(:name, :content, :end_at)
     end
 end
