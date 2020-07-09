f6a6dcbce915c646b2bbb09414e451046ef4ae87
asaki kusano <darumade4@gmail.com>
2020-07-09 22:33:57 +0900

    user createと同時にログイン

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = 'ログインしました'
       redirect_to user_path(@user.id)
-      session[:user_id] = user.id
     else
       render :new
     end
