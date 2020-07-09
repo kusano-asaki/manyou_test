9595793957239205c6d80d9b80536112cd345735
asaki kusano <darumade4@gmail.com>
2020-07-09 10:26:44 +0900

    completed enumへ変更

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,5 @@ class Task < ApplicationRecord
   scope :completed_like, -> (query) { where(completed: query)}
 
   enum priority: { 低: 0, 中: 1, 高: 2 }
+  enum completed: { 未着手: 0, 着手中: 1, 完了: 2 }
 end
